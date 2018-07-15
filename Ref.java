ec6dbf5ee811ab6b3d730b2a8f9d33872d1b1309
TheHemny <41271478+TheHemny@users.noreply.github.com>
2018-07-15 18:15:38 -0400

    Add files via upload

---
+++ Ref.java
@@ -0,0 +1,6 @@
+package Kanye.Kanye_BOT;
+
+public class Ref {
+	public static final String token = "NDY4MTI4NjQ1NDQyMTc0OTg3.Di0rHw.YlQgsvtrpdHLk8spu3hbWRWwvH0";
+	public static final String prefix = ">";
+}
